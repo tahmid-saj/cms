6badd385274256335db0dc39b59d4f334af42bf4
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-14 22:53:48 -0400

    Adding table for posts.php

---
+++ admin/posts.php
@@ -0,0 +1,64 @@
+<?php include "includes/admin_header.php";?>
+
+    <div id="wrapper">
+
+    <?php
+        include "includes/admin_navigation.php";
+    ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome to Admin
+                            <small>Author</small>
+                        </h1>
+
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Author</th>
+                                    <th>Title</th>
+                                    <th>Category</th>
+                                    <th>Status</th>
+                                    <th>Image</th>
+                                    <th>Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+
+                                    
+                                </tr>
+                            </thead>  
+
+                            <tbody>
+                                <tr>
+                                    <th>10</th>
+                                    <th>Edwin Diaz</th>
+                                    <th>Bootstrap framework</th>
+                                    <th>Bootstrap</th>
+                                    <th>Status</th>
+                                    <th>Image</th>
+                                    <th>Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+                                </tr>
+                            </tbody>
+
+                        </table>
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+
+
+<?php include "includes/admin_footer.php"?>
