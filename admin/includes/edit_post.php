5491fde87ce66309ca9b4015b28c53415e4a078a
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-15 02:45:05 -0400

    Updating edit post form

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -57,8 +57,23 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     </div>
 
     <div class="form-group">
-        <label for="post_category">Post Category Id</label>
-        <input value="<?php echo $post_category_id; ?>" type="text" class="form-control" name="post_category_id">
+        <select name="post_category" id="post_category">
+            <?php
+                $query = "select * from categories";
+                $select_categories = mysqli_query($connection, $query);
+
+                confirmQuery($select_categories);
+    
+                while ($row = mysqli_fetch_assoc($select_categories)) {
+                    $cat_id = $row['cat_id'];
+                    $cat_title = $row['cat_title'];
+
+                    echo "<option value='{$cat_id}'>{$cat_title}</option>";
+
+                }
+
+            ?>
+        </select>
     </div>
 
     <div class="form-group">
@@ -72,8 +87,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     </div>
 
     <div class="form-group">
-        <label for="post_image">Post Image</label>
-        <input value="<?php echo $post_image; ?>" type="file" name="image">
+        <img width="100" src="../images/<?php echo $post_image; ?>" alt="post_image">
     </div>
 
     <div class="form-group">
@@ -83,7 +97,8 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
     <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea value="<?php echo $post_content; ?>" class="form-control" name="post_content" id="" cols="30" rows="10">
+        <textarea class="form-control" name="post_content" id="" cols="30" rows="10">
+        <?php echo $post_content; ?>
         </textarea>
     </div>
 
