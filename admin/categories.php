acdd7a74d1bedeb09589e975804f7b42a5b62b30
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-14 22:12:46 -0400

    Code cleanup for updating / editing categories.php

--- admin/categories.php
+++ admin/categories.php
@@ -21,21 +21,7 @@
                         <div class="col-xs-6">
 
                             <?php
-                                if (isset($_POST['submit'])) {
-                                    $cat_title = $_POST['cat_title'];
-
-                                    if ($cat_title == "" || empty($cat_title)) {
-                                        echo "This field should not be empty";
-                                    } else {
-                                        $query = "insert into categories(cat_title)";
-                                        $query .= "value('{$cat_title}')";
-                                        $create_category_query = mysqli_query($connection, $query);
-
-                                        if (!$create_category_query) {
-                                            die("QUERY FAILED" . mysqli_error($connection));
-                                        }
-                                    }
-                                }
+                                insert_categories();
                             ?>
 
                             <form action="" method="post">
