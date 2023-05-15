6eb7e64cddfd2faaf0b9885963b090fa53b0f349
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-15 04:01:27 -0400

    Modifying edit_post to update posts

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -4,7 +4,7 @@ if (isset($_GET["p_id"])) {
     $the_post_id = $_GET["p_id"];
 }
 
-$query = "select * from posts";
+$query = "select * from posts where post_id = $the_post_id";
 $select_posts_by_id = mysqli_query($connection, $query);
 
 while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
@@ -18,6 +18,47 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     $post_comment_count = $row['post_comment_count'];
     $post_date = $row['post_date'];
     $post_content = $row['post_content'];
+}
+
+if (isset($_POST["update_post"])) {
+    $post_title = $_POST['post_title'];
+    $post_author = $_POST['post_author'];
+    $post_category_id = $_POST['post_category'];
+    $post_status = $_POST['post_status'];
+    $post_image = $_FILES['image']['name'];
+    $post_tags = $_POST['post_tags'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+    $post_content = $_POST['post_content'];
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    if (empty($post_image)) {
+        $query = "select * from posts where post_id = {$the_post_id}";
+        $select_image = mysqli_query($connection, $query);
+
+        while ($row = mysqli_fetch_array($select_image)) {
+            $post_image = $row["post_image"];
+        }
+    }
+
+    $query = "update posts set ";
+    $query .= "post_title = '{$post_title}', ";
+    $query .= "post_category_id = '{$post_category_id}', ";
+    $query .= "post_date = now(), ";
+    $query .= "post_author = '{$post_author}', ";
+    $query .= "post_status = '{$post_status}', ";
+    $query .= "post_tags = '{$post_tags}', ";
+    $query .= "post_content = '{$post_content}', ";
+    $query .= "post_image = '{$post_image}' ";
+    $query .= "where post_id = '{$the_post_id}'";
+
+    $update_post = mysqli_query($connection, $query);
+
+    confirmQuery($update_post);
+
+    if (!$update_post) {
+        die("Query failed " . mysqli_error($connection));
+    }
 }
 
 // if (isset($_POST["create_post"])) {
@@ -53,7 +94,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
     <div class="form-group">
         <label for="title">Post Title</label>
-        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="title">
+        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="post_title">
     </div>
 
     <div class="form-group">
@@ -78,7 +119,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
 
     <div class="form-group">
         <label for="author">Post Author</label>
-        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="author">
+        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author">
     </div>
 
     <div class="form-group">
@@ -87,7 +128,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     </div>
 
     <div class="form-group">
-        <img width="100" src="../images/<?php echo $post_image; ?>" alt="post_image">
+        <img width="100" src="../images/<?php echo $post_image; ?>" alt="image">
     </div>
 
     <div class="form-group">
@@ -103,7 +144,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     </div>
 
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+        <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
     </div>
 
 </form>
