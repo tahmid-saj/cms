5350c40a5103828e83d683a18d4dff9074defe86
Tahmid Sajin <tahmid.saj@gmail.com>
2023-05-27 00:16:32 -0400

    Updating users page

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -1,30 +1,33 @@
 <?php
 
-if (isset($_POST["create_post"])) {
-    $post_title = $_POST["title"];
-    $post_author = $_POST["author"];
-    $post_category_id = $_POST["post_category"];
-    $post_status = $_POST["post_status"];
+if (isset($_POST["create_user"])) {
+    // $user_id = $_POST["user_id"];
+    $user_firstname = $_POST["user_firstname"];
+    $user_lastname = $_POST["user_lastname"];
+    $user_role = $_POST["user_role"];
 
-    $post_image = $_FILES["image"]['name'];
-    $post_image_temp = $_FILES["image"]['tmp_name'];
+    // $post_image = $_FILES["image"]['name'];
+    // $post_image_temp = $_FILES["image"]['tmp_name'];
 
-    $post_tags = $_POST["post_tags"];
-    $post_content = $_POST["post_content"];
-    $post_date = date("d-m-y");
+    $username = $_POST["username"];
+    $user_email = $_POST["user_email"];
+    $user_password = $_POST["user_password"];
+
+    echo $user_firstname;
+    // $post_date = date("d-m-y");
     // $post_comment_count = 4;
 
-    move_uploaded_file($post_image_temp, "../images/$post_image");
+    // move_uploaded_file($post_image_temp, "../images/$post_image");
 
-    $query = "insert into posts(post_category_id, post_title, post_author, post_date, post_image, 
-    post_content, post_tags, post_status) ";
+    // $query = "insert into posts(post_category_id, post_title, post_author, post_date, post_image, 
+    // post_content, post_tags, post_status) ";
 
-    $query .= "values ('{$post_category_id}', '{$post_title}', '{$post_author}', now(), '{$post_image}',
-    '{$post_content}', '{$post_tags}', '{$post_status}')";
+    // $query .= "values ('{$post_category_id}', '{$post_title}', '{$post_author}', now(), '{$post_image}',
+    // '{$post_content}', '{$post_tags}', '{$post_status}')";
 
-    $create_post_query = mysqli_query($connection, $query);
+    // $create_post_query = mysqli_query($connection, $query);
 
-    confirmQuery($create_post_query);
+    // confirmQuery($create_post_query);
 }
 
 ?>
@@ -43,21 +46,12 @@ if (isset($_POST["create_post"])) {
 
     <div class="form-group">
         <select name="user_role" id="">
-            <?php
-                $query = "select * from users";
-                $select_users = mysqli_query($connection, $query);
 
-                confirmQuery($select_users);
-    
-                while ($row = mysqli_fetch_assoc($select_users)) {
-                    $user_id = $row['user_id'];
-                    $user_role = $row['user_role'];
+            <option value="subscriber">Select Options</option>
+            <option value="admin">Admin</option>
+            <option value="subscriber">Subscriber</option>
 
-                    echo "<option value='{$user_id}'>{$user_role}</option>";
 
-                }
-
-            ?>
         </select>
     </div>
 
@@ -79,7 +73,7 @@ if (isset($_POST["create_post"])) {
 
     <div class="form-group">
         <label for="post_content">Password</label>
-        <input type="email" class="form-control" name="user_password">
+        <input type="password" class="form-control" name="user_password">
         </textarea>
     </div>
 
--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -97,6 +97,30 @@ if (isset($_POST["update_post"])) {
         <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="post_title">
     </div>
 
+    
+
+    <div class="form-group">
+        <select name="user_role" id="">
+            <?php
+                $query = "select * from users";
+                $select_users = mysqli_query($connection, $query);
+
+                confirmQuery($select_users);
+    
+                while ($row = mysqli_fetch_assoc($select_users)) {
+                    $user_id = $row['user_id'];
+                    $user_role = $row['user_role'];
+
+                    echo "<option value='{$user_id}'>{$user_role}</option>";
+
+                }
+
+            ?>
+        </select>
+    </div>
+
+
+
     <div class="form-group">
         <select name="post_category" id="post_category">
             <?php
