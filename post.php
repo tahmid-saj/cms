6cf93e733e37fa1b92a7c1d7d7fc93c3e51e7856
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-03 18:17:24 -0400

    Updating post.php

--- post.php
+++ post.php
@@ -54,7 +54,6 @@ include "includes/navigation.php";
                 <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
                 <hr>
                 <p><?php echo $post_content ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
 
