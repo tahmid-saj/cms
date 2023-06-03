aa7d0a769ede51ffb29957fe3d3f1ed05efb9991
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-03 14:28:53 -0400

    Updating user data

--- admin/profile.php
+++ admin/profile.php
@@ -21,6 +21,47 @@
     }
 ?>
 
+<?php
+    if (isset($_POST["edit_user"])) {
+        // $user_id = $_POST["user_id"];
+        $user_firstname = $_POST["user_firstname"];
+        $user_lastname = $_POST["user_lastname"];
+        $user_role = $_POST["user_role"];
+    
+        // $post_image = $_FILES["image"]['name'];
+        // $post_image_temp = $_FILES["image"]['tmp_name'];
+    
+        $username = $_POST["username"];
+        $user_email = $_POST["user_email"];
+        $user_password = $_POST["user_password"];
+    
+        echo $user_firstname;
+        // $post_date = date("d-m-y");
+        // $post_comment_count = 4;
+    
+        // move_uploaded_file($post_image_temp, "../images/$post_image");
+    
+        $query = "update users set ";
+        $query .= "user_firstname = '{$user_firstname}', ";
+        $query .= "user_lastname = '{$user_lastname}', ";
+        $query .= "user_role = '{$user_role}', ";
+        $query .= "username = '{$username}', ";
+        $query .= "user_email = '{$user_email}', ";
+        $query .= "user_password = '{$user_password}' ";
+        $query .= "where username = '{$username}'";
+    
+        $edit_user = mysqli_query($connection, $query);
+    
+        confirmQuery($edit_user);
+    
+        if (!$edit_user) {
+            die("Query failed " . mysqli_error($connection));
+        }
+    }
+    
+
+?>
+
 
     <div id="wrapper">
 
