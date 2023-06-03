70a35fdeefd7638dae1277808c6f852f4d378c7a
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-03 19:57:28 -0400

    Adding wysiwyg editor

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -78,7 +78,7 @@ if (isset($_POST["create_post"])) {
 
     <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea class="form-control" name="post_content" id="" cols="30" rows="10">
+        <textarea class="form-control" name="post_content" id="summernote" cols="30" rows="10">
         </textarea>
     </div>
 
