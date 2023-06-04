29f7ae66b035f059de1a0509b623d48ee0940abd
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-04 02:24:31 -0400

    Adding view post to table

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -69,6 +69,7 @@ if (isset($_POST["checkBoxArray"])) {
             <th>Tags</th>
             <th>Comments</th>
             <th>Date</th>
+            <th>View Post</th>
             <th>Edit</th>
             <th>Delete</th>
 
@@ -124,7 +125,10 @@ if (isset($_POST["checkBoxArray"])) {
             echo "<td>{$post_tags}</td>";
             echo "<td>{$post_comment_count}</td>";
             echo "<td>{$post_date}</td>";
+
+            echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
             echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
+
             echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
             echo "</tr>";
         }
