29bb9a6e6f9d5433944010a7548ac179bf4da359
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-04 13:45:40 -0400

    Updating registration.php

--- registration.php
+++ registration.php
@@ -3,6 +3,13 @@
 <?php  include "includes/db.php"; ?>
  <?php  include "includes/header.php"; ?>
 
+<?php
+
+if (isset($_POST["submit"])) {
+    echo "Its working";
+}
+
+?>
 
     <!-- Navigation -->
     
