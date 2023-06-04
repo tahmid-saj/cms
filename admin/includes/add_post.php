ec49e80958846dc1fbbe1808b18d86b6b9900897
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-04 00:40:37 -0400

    Adding bulk options to posts

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -63,7 +63,14 @@ if (isset($_POST["create_post"])) {
 
     <div class="form-group">
         <label for="post_status">Post Status</label>
-        <input type="text" class="form-control" name="post_status">
+
+        <select name="post_status" id="">
+            <option value="draft">Select options</option>
+            <option value="published">Publish</option>
+            <option value="draft">Draft</option>
+
+        </select>
+
     </div>
 
     <div class="form-group">
