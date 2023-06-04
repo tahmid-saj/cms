c36d05d23ddbe2e2e00938f32781801771ce1d27
Tahmid Sajin <tahmid.saj@gmail.com>
2023-06-03 20:02:36 -0400

    Adding clickable links to index.php

--- index.php
+++ index.php
@@ -48,11 +48,17 @@ include "includes/navigation.php";
                             by <a href="index.php"><?php echo $post_author ?></a>
                         </p>
                         <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date ?></p>
+
+                    <a href="post.php?p_id=<?php echo $post_id;?>">
+
                         <hr>
                         <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
+
+                    </a>
+
                         <hr>
                         <p><?php echo $post_content ?></p>
-                        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                        <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id;?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                         <hr>
 
